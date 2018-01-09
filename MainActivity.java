49ef41bda07b7bbc9d140589b16f0111ecbb90b7
simplydikka <33466126+simplydikka@users.noreply.github.com>
2018-01-09 13:35:35 +0200

    MainActivity.java
    Added a code snippet for full screen

---
+++ MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.happybirthday;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+    }
+}
